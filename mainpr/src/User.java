0069ad89a3f1483a43c726fd1f8a7b023ed50a49
JubinKS333 <jubinks3@gmail.com>
2019-02-02 12:20:58 -0500

    USER CODE UPDATION

--- mainpr/src/User.java
+++ mainpr/src/User.java
@@ -2,33 +2,17 @@ import java.util.ArrayList;
 //import java.util.Arrays;
 
 public class User {
-    ArrayList<String> userid=new ArrayList<String>();
 
-    public ArrayList<String> getUserid() {
-        return u                    serid;
-    }
+    public static void main(String[] args)
 
-    public void setUserid(ArrayList<String> userid) {
-        this.userid = userid;
-    }
-
-    public ArrayList<String> getPassword() {
-        return password;
-    }
-
-    public void setPassword(ArrayList<String> password) {
-        this.password = password;
-    }
-
-    ArrayList<String> password=new ArrayList<String>();
-    String loginStatus;
-    //String userid;
-    //String password;
-    public boolean verifyLogin(String uid, String pass)
     {
-        if(userid.contains(uid) && password.contains(pass))
-            return true;
-        else
-            return false;
+    private String userId ;
+    private String password;
+    private String loginStatus;
+
+    public static void verifyLogin()
+    {
+
     }
+
 }
